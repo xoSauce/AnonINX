c59b1f260208a772950961a202070e8c28d22621
xoSauce <sergiu.ferentz@gmail.com>
2017-02-15 22:52:57 +0000

    indentation issue

--- m_keys_server.py
+++ m_keys_server.py
@@ -3,9 +3,9 @@ from key_listener import KeyListener
 from requestCreator import PortEnum
 def main():
 	portEnum = PortEnum
-    broker = Broker()
-    key_listener = KeyListener(portEnum.broker.value, broker)
-    key_listener.listen()
+	broker = Broker()
+	key_listener = KeyListener(portEnum.broker.value, broker)
+	key_listener.listen()
 
 if __name__ == '__main__':
     main()
