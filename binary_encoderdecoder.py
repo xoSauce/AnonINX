6def30331ff39031594db150e284258f7244a762
xosauce <sergiu.ferentz@gmail.com>
2017-04-21 21:43:00 +0100

    adding optimizer class

---
+++ binary_encoderdecoder.py
@@ -0,0 +1,35 @@
+from petlib.pack import encode, decode
+
+class BinaryEncoderDecoder:
+    def __init__(self, encoder_size = 64):
+        self.size = encoder_size
+        pass
+    def encode_binary(self, l):
+        string_conversion = "".join([str(x) for x in l])
+        string_conversion = bytearray(string_conversion.encode())
+        result = []
+        while string_conversion:
+            temporary_encoder = bytearray(string_conversion[:self.size])
+            x = int(temporary_encoder, 2)
+            petlib_encode = encode(x)
+            string_conversion = string_conversion[self.size:]
+            result.append(petlib_encode)
+        return encode(result)
+    def decode_binary(self, e):
+        e = decode(e)
+        result = ""
+        for x in e:
+            petlib_decode = decode(x)
+            binary = "{0:b}".format(petlib_decode)
+            result += binary
+        l = []
+        for x in result:
+            l.append(int(x))
+        return l
+if __name__ == '__main__':
+    enc = BinaryEncoderDecoder()
+    array = [1,0,0,1] * 1000000
+    encoding =  enc.encode_binary(array)
+    decoding = enc.decode_binary(encoding)
+    print(len(decoding), len(encoding))
+    assert(decoding == array)
